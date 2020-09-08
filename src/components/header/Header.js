9c2b04f237157df5a3b33209f0a4544598b61cec
Diogo da Silva França Marins <dmarins@braspag.com.br>
2020-09-08 00:21:15 -0300

    componente de navegação

---
+++ src/components/header/Header.js
@@ -0,0 +1,19 @@
+import React from "react";
+import styles from "./Header.module.css";
+import { NavLink } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <nav className={styles.header}>
+      <ul>
+        <li>
+          <NavLink className={styles.link} to="/" end>
+            Filmes
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Header;
